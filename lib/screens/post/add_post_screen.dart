11bcdb3e4217bb0f63efe5ef6d64f0a8db09436e
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-11-04 18:10:09 +0900

    addTagを修正

--- lib/screens/post/add_post_screen.dart
+++ lib/screens/post/add_post_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:nijimas/core/delegates/search_tag_delegate.dart';
 import 'package:nijimas/core/providers/nijimas_notifier_provider.dart';
 import 'package:nijimas/core/utils.dart';
 import 'package:nijimas/widgets/post/tag_chip.dart';
@@ -12,6 +11,7 @@ class AddPostScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final useSelectedTags = useState<List<String>>([]);
+    final useTagController = useTextEditingController();
 
     final nijimas = ref.watch(nijimasProvider)!.section;
 
@@ -30,42 +30,50 @@ class AddPostScreen extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: GestureDetector(
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.add),
-              SizedBox(width: 8),
-              Text(
-                "タグを追加",
-                style: TextStyle(color: Colors.black),
-              ),
-            ],
-          ),
-          onTap: () {
-            showSearch(
-                context: context,
-                delegate: SearchTagDelegate(ref: ref, addTag: addTag));
-          },
-        ),
+        centerTitle: false,
+        title: Text(nijimas, style: const TextStyle(color: Colors.black)),
       ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: useSelectedTags.value.map((tag) {
-                return TagChip(
-                  tagName: tag,
-                  removeTag: (tagToRemove) => removeTag(tagToRemove),
-                );
-              }).toList(),
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: TextField(
+              controller: useTagController,
+              decoration: const InputDecoration(
+                filled: true,
+                hintText: "タグを追加",
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(18),
+              ),
+              onSubmitted: (value) {
+                if (value.isNotEmpty) {
+                  if (value.length <= 20) {
+                    addTag(value);
+                    useTagController.clear();
+                  } else {
+                    showErrorSnackBar(context, "タグは20文字以内で入力してください。");
+                  }
+                }
+              },
             ),
           ),
-          Text(nijimas)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Wrap(
+                spacing: 8,
+                runSpacing: 8,
+                children: useSelectedTags.value.map((tag) {
+                  return TagChip(
+                    tagName: tag,
+                    removeTag: (tagToRemove) => removeTag(tagToRemove),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
         ],
       ),
     );
