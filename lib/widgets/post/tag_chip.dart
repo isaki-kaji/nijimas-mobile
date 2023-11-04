66deb8f6978e6443f5b9f400f68202b3bb251748
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-11-04 18:48:57 +0900

    TagChipを修正

--- lib/widgets/post/tag_chip.dart
+++ lib/widgets/post/tag_chip.dart
@@ -10,27 +10,25 @@ class TagChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: MyColors.whiteColor,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.horizontal(
-          left: Radius.circular(30),
-          right: Radius.circular(30),
+    return GestureDetector(
+      onTap: () => removeTag(tagName),
+      child: Card(
+        color: MyColors.whiteColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.horizontal(
+            left: Radius.circular(30),
+            right: Radius.circular(30),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-        child: IntrinsicWidth(
-          child: Row(
-            children: [
-              Text(tagName),
-              IconButton(
-                icon: const Icon(Icons.close, size: 18),
-                padding: EdgeInsets.zero, // IconButtonのパディングを0に
-                constraints: const BoxConstraints(), // サイズ制約をリセット
-                onPressed: () => removeTag(tagName),
-              ),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+          child: IntrinsicWidth(
+            child: Row(
+              children: [
+                Text(tagName),
+                const Icon(Icons.close, size: 18),
+              ],
+            ),
           ),
         ),
       ),
