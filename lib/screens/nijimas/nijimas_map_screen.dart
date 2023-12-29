b9b79128c47343dfb0ee16adb1ae795a6b0cf90d
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-12-30 00:04:46 +0900

    mapスクリーン作成開始

--- lib/screens/nijimas/nijimas_map_screen.dart
+++ lib/screens/nijimas/nijimas_map_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nijimas/widgets/nijimas/two_dimensional_grid_view.dart';
 
 class NijimasMapScreen extends HookConsumerWidget {
   const NijimasMapScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(child: Text("nijiimas_map"));
+    return Scaffold(
+        body: TwoDimensionalGridView(
+      diagonalDragBehavior: DiagonalDragBehavior.free,
+      delegate: TwoDimensionalChildBuilderDelegate(
+        maxXIndex: 20,
+        maxYIndex: 20,
+        builder: (BuildContext context, ChildVicinity vicinity) {
+          return Container(
+            color: vicinity.xIndex.isEven && vicinity.yIndex.isEven
+                ? Colors.amber[50]
+                : (vicinity.xIndex.isOdd && vicinity.yIndex.isOdd
+                    ? Colors.purple[50]
+                    : Colors.white),
+            height: 200,
+            width: 200,
+          );
+        },
+      ),
+    ));
   }
 }
