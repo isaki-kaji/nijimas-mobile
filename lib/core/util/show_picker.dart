2c04c1b6ee3c4889b000885c2f2fee73ab527f82
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-05-16 16:59:59 +0900

    flutter_pickerをbottom_pickerに差し替え

--- lib/core/util/show_picker.dart
+++ lib/core/util/show_picker.dart
@@ -1,17 +1,22 @@
+import 'package:bottom_picker/bottom_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_picker/flutter_picker.dart';
 import 'package:nijimas/core/enum/main_category.dart';
+import 'package:nijimas/core/theme/my_color.dart';
 
 void showPicker(BuildContext context, void Function(String) tapEvent) {
-  Picker picker = Picker(
-      height: 300.0,
-      adapter: PickerDataAdapter<String>(pickerData: mainCategoryNames()),
-      changeToFirst: true,
-      textAlign: TextAlign.left,
-      itemExtent: 50.0,
-      textStyle: const TextStyle(color: Colors.black, fontSize: 30),
-      onConfirm: (Picker picker, List value) {
-        tapEvent(picker.getSelectedValues().first);
-      });
-  picker.showModal(context);
+  BottomPicker(
+    pickerTitle: const SizedBox.shrink(),
+    height: 300.0,
+    buttonContent: const Icon(Icons.check, color: MyColors.white),
+    buttonSingleColor: MyColors.lightGreen,
+    items: mainCategoryNames()
+        .map((name) => Text(name,
+            style: const TextStyle(color: Colors.black, fontSize: 30)))
+        .toList(),
+    dismissable: true,
+    itemExtent: 50.0,
+    onSubmit: (index) {
+      tapEvent(mainCategoryNames()[index]);
+    },
+  ).show(context);
 }
