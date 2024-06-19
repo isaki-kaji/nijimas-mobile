ec21345573b68b36189eba9697aaf533576b33dd
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-06-19 17:57:29 +0900

    feat: SpendingDistributionCardを実装

--- lib/presentation/screen/user/user_detail_screen.dart
+++ lib/presentation/screen/user/user_detail_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nijimas/application/state/own_user_detail_provider.dart';
+import 'package:nijimas/core/enum/main_category.dart';
 import 'package:nijimas/core/theme/text_style.dart';
 import 'package:nijimas/domain/response/user_response.dart';
-import 'package:nijimas/presentation/widget/user/spending_rates_card.dart';
+import 'package:nijimas/presentation/widget/user/spending_distribution_card.dart';
 import 'package:nijimas/presentation/widget/user/switch_circle_avatar.dart';
 
 class UserDetailScreen extends ConsumerWidget {
@@ -84,14 +85,13 @@ class UserDetailScreen extends ConsumerWidget {
                       Text(
                           user.selfIntro ?? '彼女の笑顔はまるで春の陽射しのように温かく、心を和ませてくれる。'),
                       const SizedBox(height: 20),
-                      const SpendingRatesCard(
-                        title: "費目別の支出の割合",
-                        spendingRates: {
-                          "食費": 20.0,
-                          "交通費": 20.0,
-                          "交際費": 20.0,
-                          "趣味": 20.0,
-                          "その他": 20.0,
+                      const SpendingDistributionCard(
+                        title: "支出の割合",
+                        spendingDistribution: {
+                          MainCategory.hobbies: 0.4,
+                          MainCategory.food: 0.2,
+                          MainCategory.transport: 0.2,
+                          MainCategory.entertainment: 0.2,
                         },
                       ),
                     ],
