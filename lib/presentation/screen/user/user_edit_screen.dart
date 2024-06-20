50ce5d7a7bb96ea12b793d5a9aa7ec7a5851ddf9
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2024-06-20 18:06:49 +0900

    feat: 文字入力欄について実装

--- lib/presentation/screen/user/user_edit_screen.dart
+++ lib/presentation/screen/user/user_edit_screen.dart
@@ -8,31 +8,73 @@ import 'package:nijimas/presentation/widget/common/loader.dart';
 import 'package:nijimas/presentation/widget/user/switch_circle_avatar.dart';
 
 class UserEditScreen extends HookConsumerWidget {
-  const UserEditScreen({super.key, required this.uid});
+  UserEditScreen({super.key, required this.uid});
   final String uid;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final useNameController = useTextEditingController();
+    final user = ref.watch(userProfileInfoProvider(uid));
+    final useNameController =
+        useTextEditingController(text: user.valueOrNull!.username);
+    final useSelfIntroController =
+        useTextEditingController(text: user.valueOrNull!.selfIntro);
     return Scaffold(
       appBar: AppBar(),
-      body: ref.watch(userProfileInfoProvider(uid)).when(
+      body: user.when(
         data: (data) {
-          return Center(
-            child: Column(
-              children: [
-                SwitchCircleAvatar(
-                  imageUrl: data!.profileImageUrl,
-                  radius: Sizing.widthByMQ(context, 0.2),
-                  onTap: () {},
-                ),
-                TextFormField(
-                  controller: useNameController,
-                  decoration: const InputDecoration(
-                    labelText: "名前",
+          return Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                children: [
+                  const Spacer(flex: 2),
+                  SwitchCircleAvatar(
+                    imageUrl: data!.profileImageUrl,
+                    radius: Sizing.widthByMQ(context, 0.2),
+                    onTap: () {},
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 80,
+                    width: Sizing.widthByMQ(context, 0.6),
+                    child: TextFormField(
+                      controller: useNameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                      ),
+                      validator: (name) {
+                        if (name!.isEmpty) {
+                          return "名前を入力してください";
+                        }
+                        if (name.length < 2 || name.length > 15) {
+                          return "2文字以上15文字以下で入力してください";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: SizedBox(
+                      width: Sizing.widthByMQ(context, 0.9),
+                      child: TextFormField(
+                        controller: useSelfIntroController,
+                        maxLines: 6,
+                        maxLength: 200,
+                        decoration: const InputDecoration(
+                          filled: true,
+                          hintText: "自己紹介を入力してください",
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(18),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(flex: 3),
+                ],
+              ),
             ),
           );
         },
@@ -49,7 +91,10 @@ class UserEditScreen extends HookConsumerWidget {
           Icons.done,
           color: MyColors.white,
         ),
-        onPressed: () {},
+        onPressed: () {
+          final formState = _formKey.currentState!;
+          formState.validate();
+        },
       ),
     );
   }
